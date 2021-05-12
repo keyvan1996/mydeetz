8061dbf6415e98a9b84a3770dbfdb5cfef9a366f
Keyvan Shabani <kshabani@ggc.edu>
2021-05-12 12:04:06 -0400

    adding background

---
+++ build/precache-manifest.f65a85886dc10e5deafff4ade2902e27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5b6b2d53d9b22887498",
+    "url": "/mydeetz/static/css/main.53bc0172.chunk.css"
+  },
+  {
+    "revision": "b5b6b2d53d9b22887498",
+    "url": "/mydeetz/static/js/main.b5b6b2d5.chunk.js"
+  },
+  {
+    "revision": "fe06a49a7dd99562f362",
+    "url": "/mydeetz/static/js/runtime~main.fe06a49a.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/mydeetz/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "e93d2aa9a0f8530f9217f6e75ac022e9",
+    "url": "/mydeetz/index.html"
+  }
+];
--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -13,23 +13,23 @@ export default class Introduction extends Component {
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner js-fullheight">
-                        <div className="desc">
+                        <div className="desc" className="mine">
                           <h1>Hi! <br />I'm Keyvan</h1>
-                          <p><a className="btn btn-primary btn-learn" href="" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1LCLHoJDTX3REsVIW2Eb8l7FhhJTtpMHN/view?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </li>
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+              <li style={{backgroundImage: 'url(images/img-bg1.jpeg)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
-                        <div className="desc">
-                          <h1>I love building<br /> THINGS !!</h1>
+                        <div className="desc" className="mine">
+                          <h1>I love building<br /> THINGS!</h1>
                           <p><a className="btn btn-primary btn-learn" href="https://github.com/keyvan1996" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
                         </div>
                       </div>
@@ -37,13 +37,13 @@ export default class Introduction extends Component {
                   </div>
                 </div>
               </li>
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+              <li style={{backgroundImage: 'url(images/img-bg2.jpeg)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
-                        <div className="desc">
+                        <div className="desc" className="mine">
                           <h1>I often <br/>Write ... </h1>
                           <p><a className="btn btn-primary btn-learn" href="https://www.google.com/" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
                         </div>
